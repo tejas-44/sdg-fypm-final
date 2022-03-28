f87041243afe28d4075c071b4e67d7fed2aa6565
Tejas Shelke <69912804+tejas-44@users.noreply.github.com>
2022-03-28 14:19:40 +0530

    UI Changes

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# sdg-fypm
-Final Year Project Management SDG Project
+This project is a portal for the final year students to upload their submission projects. There are improvements required in the UI. This project is beginner friendly and i will request all the beginners to take a look at the issues and contact me regarding the project.
--- js/firebase.js
+++ js/firebase.js
@@ -46,3 +46,72 @@ function signup() {
 
 
 }
+var db = firebase.firestore();
+
+db.collection("mentor").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        const mentorname = document.querySelector("#mentorname");
+        mentorname.innerHTML += doc.data().name;
+
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+
+
+
+db.collection("student").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        const student = document.querySelector("#studentdiv");
+        student.innerHTML += "<h3 class='member'>" + doc.data().name + "</h3>";
+
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+
+db.collection("sub").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        const viewMentorName = document.querySelector("#viewsubmentorname");
+        viewMentorName.innerHTML += doc.data().guidename;
+
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+db.collection("sub").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        const abstractData = document.querySelector("#viewabstract");
+        abstractData.innerHTML += doc.data().abstractData;
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+db.collection("sub").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        const viewprojname = document.querySelector("#viewprojname");
+        viewprojname.innerHTML += doc.data().projectname;
+
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+
+function submit() {
+    var projName = document.getElementById("subprojname").value;
+    var guideName = document.getElementById("subguidename").value;
+    var abstract = document.getElementById("subabstract").value;
+
+    db.collection("sub").doc(projName).set({
+        projectname: projName,
+        guidename: guideName,
+        abstractData: abstract
+    })
+        .then(() => {
+            console.log("Document successfully written!");
+            window.location.href = "viewsub.html"
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+}
